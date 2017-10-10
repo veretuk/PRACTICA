1a669ba2d06347908c6f1b101c24166d985859c9
veretuk <mishablanski69@gmail.com>
2017-10-10 10:49:27 +0300

    Update Final.cpp

--- Final.cpp
+++ Final.cpp
@@ -3,7 +3,7 @@
 #include <string.h> 
 #include <locale.h> 
 
-#define N 2
+#define N 1
 
 typedef struct {
 	char name[3];
@@ -18,16 +18,16 @@ int main() {
 	int key;
 	setlocale(LC_CTYPE, "rus");
 	ZNAK arrayZnak[N];
-	printf("Aalacnl %i crdcnle\n", N);
+	printf("Input %i  name\n", N);
 	setZod(arrayZnak);
 again:
-	puts("1 - Ddineindlnu eiinrenu\n2 - Auadrnu di cireo ciacreo\n3 - Creii?cnu draino");
+	puts("1 -Out put zodiak\n3 - Closed");
 	scanf("%i", &key);
 	switch (key)
 	{
 	case 1: printZod(arrayZnak);	break;
-	
-	case 3: puts("Ndrncai cr cndieuciaricl ddiadreeeu"); return 0;
+
+	case 3: puts("Closed project"); return 0;
 	}
 	goto again;
 	return 0;
@@ -36,13 +36,13 @@ again:
 void setZod(ZNAK* arrayZnak) {
 	setlocale(LC_CTYPE, "rus");
 	for (int i = 0; i<N; i++) {
-		printf("Aalacnl ce?: ");
+		printf("Input Name: ");
 		//scanf("%s", &arrayZnak->name);
 		scanf("%s %s %s", &arrayZnak->name[0], &arrayZnak->name[1], &arrayZnak->name[2]);
 
-		printf("Aalacnl ciacre: ");
+		printf("Input zodiak: ");
 		scanf("%s", &arrayZnak->zodiak);
-		printf("aa.ee.aa: ");
+		printf("Bday --.--.--: ");
 		scanf("%d.%d.%d", &arrayZnak->bday[0], &arrayZnak->bday[1], &arrayZnak->bday[2]);
 		printf("\n\n");
 		arrayZnak++;
@@ -53,7 +53,7 @@ void printZod(ZNAK* arrayZnak) {
 	setlocale(LC_CTYPE, "rus");
 	for (int i = 0; i<N; i++)
 	{
-		printf("Ce?: %s %s %s\nCiaccre: %s\nAD : %d.%d.%d\n\n", &arrayZnak->name[0], &arrayZnak->name[1], &arrayZnak->name[2], arrayZnak->zodiak, arrayZnak->bday[0], arrayZnak->bday[1], arrayZnak->bday[2]);
+		printf("Name: %s %s %s\nZodiak: %s\nBday : %d.%d.%d\n\n", &arrayZnak->name[0], &arrayZnak->name[1], &arrayZnak->name[2], arrayZnak->zodiak, arrayZnak->bday[0], arrayZnak->bday[1], arrayZnak->bday[2]);
 		arrayZnak++;
 	}
 }
